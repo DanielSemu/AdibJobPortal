433d8bfb8ebc1b9004da3a223b73d09de16fa645
DanieSemu <danielsemu06@gmail.com>
2025-02-02 07:51:27 +0300

    sundays submission

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
   };
   return (
     <>
-      <nav className="bg-white dark:bg-[#007dda] fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+      <nav className="bg-[#007dda] fixed w-full z-20 top-0 start-0 ">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link
             to="/"
@@ -62,7 +62,7 @@ const Navbar = () => {
             className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
             id="navbar-sticky"
           >
-            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-[#007dda] dark:border-gray-700">
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border  rounded-lg  bg-[#007dda]  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
               <li>
                 <Link
                   to="/"
