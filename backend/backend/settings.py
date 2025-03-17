7fc36df35de31202ee64793bcbefd25f418fb495
DanielSemu <danielsemu06@gmail.com>
2025-03-17 08:28:49 +0300

    first updates of monday

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,24 +96,24 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    #  'default': {
-    #     'ENGINE': 'mssql',
-    #     'NAME': 'ADIB_JobPortal',
-    #     'USER': 'sa',
-    #     'PASSWORD': 'Adib@Te5t',
-    #     'HOST': 'INAPP-DB-SRVR\\ADIBTESTDBSERVER',  # Named instance
-    #     'PORT': '',  # Must be empty for named instances
-    #     'OPTIONS': {
-    #         'driver': 'ODBC Driver 17 for SQL Server',
-    #         # Remove extra_params or use valid encryption setting
-    #         'encrypt': 'optional',  # Lowercase, valid for some drivers
-    #         'trust_server_certificate': 'yes'  # If needed
-    #     },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+     'default': {
+        'ENGINE': 'mssql',
+        'NAME': 'ADIB_JobPortal',
+        'USER': 'sa',
+        'PASSWORD': 'Adib@Te5t',
+        'HOST': 'INAPP-DB-SRVR\\ADIBTESTDBSERVER',  # Named instance
+        'PORT': '',  # Must be empty for named instances
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+            # Remove extra_params or use valid encryption setting
+            'encrypt': 'optional',  # Lowercase, valid for some drivers
+            'trust_server_certificate': 'yes'  # If needed
+        },
+    }
 }
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
--- frontend/src/services/axiosInstance.js
+++ frontend/src/services/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-    baseURL: 'http://192.168.75.222:8000/api/',
+    baseURL: 'http://192.168.2.32:8000/api/',
     timeout: 10000,
     headers: {
         'Accept': 'application/json',
